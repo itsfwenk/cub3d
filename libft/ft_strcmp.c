792d6178d2f6640c1c630b6672f934b17131ceea
Mathis El Habib <mel-habi@made-f0Br6s4.clusters.42paris.fr>
2024-10-09 16:47:44 +0200

    Checkers

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 14:54:39 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/10/09 16:24:24 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:34:25 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_strcmp(const char *s1, const char *s2)
 	size_t	i;
 
 	i = 0;
-	if (!s1)
-		return (!s2);
 	while (s1[i] || s2[i])
 	{
 		if (s1[i] != s2[i])
--- srcs/parser/checkers.c
+++ srcs/parser/checkers.c
@@ -6,7 +6,7 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:48:45 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/10/09 16:29:43 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:46:58 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 bool	is_correct_filename(char *filepath, char *extension)
 {
-	return (!ft_strcmp(ft_strrchr(filepath, '.'), extension));
+	char	*ext;
+
+	ext = ft_strrchr(filepath, '.');
+	if (!ext || !((ext - 1)[0]))
+		return (false);
+	return ((ext - 1)[0] != '/' && !ft_strcmp(ext, extension));
 }
