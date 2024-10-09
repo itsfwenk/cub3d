c7d427c97410a90849d16d9e17562df18204cf96
Mathis El Habib <mel-habi@made-f0Br6s4.clusters.42paris.fr>
2024-10-09 16:12:05 +0200

    .

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:46:29 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/10/09 16:03:57 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:09:27 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ void	cub3d_parser(t_cub3d *cub3d, char *map_path)
 		exit_cub3d(cub3d, EXIT_FAILURE);
 	}
 	fd = open(map_path, O_RDONLY);
-	if (fd < 0)
+	if (fd < 0 || read(fd, 0, 0) <= 0)
 	{
-		ft_print_error("Can't open this .cub file");
+		ft_print_error("Can't open this .cub file (Wrong path or a directory)");
 		exit_cub3d(cub3d, EXIT_FAILURE);
 	}
 	close(fd);
