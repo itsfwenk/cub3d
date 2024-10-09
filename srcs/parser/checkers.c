9a5b4d968133cba01aec25e67bfb5ee0bf39667d
Mathis El Habib <mel-habi@made-f0Br6s4.clusters.42paris.fr>
2024-10-09 16:28:52 +0200

    Fix path check

--- srcs/parser/checkers.c
+++ srcs/parser/checkers.c
@@ -6,8 +6,13 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:48:45 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/10/09 16:26:02 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:28:07 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+bool	is_correct_filename(char *filepath, char *extension)
+{
+	return (ft_strcmp(ft_strrchr(filepath, '.'), extension));
+}
--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,22 +6,22 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:46:29 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/10/09 16:25:53 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:28:35 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void	cub3d_parser(t_cub3d *cub3d, char *map_path)
+void	cub3d_parser(t_cub3d *cub3d, char *mappath)
 {
 	int	fd;
 
-	if (ft_strcmp(ft_strrchr(map_path, '.'), ".cub"))
+	if (!is_correct_filename(mappath, ".cub"))
 	{
 		ft_print_error("Wrong path extension (.cub)");
 		exit_cub3d(cub3d, EXIT_FAILURE);
 	}
-	fd = open(map_path, O_RDONLY);
+	fd = open(mappath, O_RDONLY);
 	if (fd < 0 || read(fd, 0, 0) < 0)
 	{
 		ft_print_error("Can't open this .cub file \
