02aa33de505d13abe055edc05b87011ad5f30554
Mathis El Habib <mel-habi@made-f0Br7s2.clusters.42paris.fr>
2024-10-15 11:26:19 +0200

    Init textures

--- srcs/raycasting/init_window.c
+++ srcs/raycasting/init_window.c
@@ -6,11 +6,37 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 18:39:12 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/10/15 10:57:52 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/15 11:26:06 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+static void	init_textures(t_cub3d *cub3d)
+{
+	int	i;
+	int	width;
+	int	height;
+
+	i = 0;
+	while (i < 4)
+	{
+		cub3d->textures[i].img_ptr = mlx_xpm_file_to_image(cub3d->connection,
+				cub3d->map->textures[i], &width, &height);
+		if (!cub3d->textures[i].img_ptr)
+			exit_cub3d(cub3d, EXIT_FAILURE);
+		else if (width != TILE_SIZE || height != TILE_SIZE)
+		{
+			ft_print_error("Bad texture size");
+			exit_cub3d(cub3d, EXIT_FAILURE);
+		}
+		cub3d->textures[i].addr = mlx_get_data_addr(cub3d->textures[i].img_ptr,
+				&cub3d->textures[i].bits_per_pixel,
+				&cub3d->textures[i].line_len,
+				&cub3d->textures[i].endian);
+		i++;
+	}
+}
 
 void	init_window(t_cub3d *cub3d)
 {
@@ -26,6 +52,7 @@ void	init_window(t_cub3d *cub3d)
 	cub3d->img.addr = mlx_get_data_addr(cub3d->img.img_ptr,
 			&cub3d->img.bits_per_pixel,
 			&cub3d->img.line_len, &cub3d->img.endian);
+	init_textures(cub3d);
 	init_raycaster(cub3d);
 	init_events(cub3d);
 	mlx_loop(cub3d->connection);
