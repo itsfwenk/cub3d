8ebd599b4397c6e5098459e50f67bc09f602ac05
Mathis El Habib <mel-habi@made-f0Br7s2.clusters.42paris.fr>
2024-10-15 10:46:14 +0200

    MLX free

--- srcs/utils/free_utils.c
+++ srcs/utils/free_utils.c
@@ -6,11 +6,24 @@
 /*   By: mel-habi <mel-habi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:11:49 by mel-habi          #+#    #+#             */
-/*   Updated: 2024/10/10 04:00:28 by mel-habi         ###   ########.fr       */
+/*   Updated: 2024/10/15 10:45:52 by mel-habi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+static void	mlx_free(t_cub3d *cub3d)
+{
+	if (cub3d->connection)
+	{
+		if (cub3d->img.img_ptr)
+			mlx_destroy_image(cub3d->connection, cub3d->img.img_ptr);
+		if (cub3d->win)
+			mlx_destroy_window(cub3d->connection, cub3d->win);
+		mlx_destroy_display(cub3d->connection);
+		free(cub3d->connection);
+	}
+}
 
 char	**free_str_tab(char	**tab)
 {
@@ -48,6 +61,7 @@ int	exit_cub3d(t_cub3d *cub3d, int exit_code)
 	}
 	if (cub3d->map->array)
 		free(cub3d->map->array);
+	mlx_free(cub3d);
 	clear_lines(cub3d->map->lines);
 	clear_gc(cub3d->gc);
 	exit(exit_code);
